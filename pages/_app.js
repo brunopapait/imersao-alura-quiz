d6f48b20cafba2f65adb7623acd91e53417ae056
brunopapait <brunohpapait@gmail.com>
2021-01-26 22:11:17 -0300

    router, lint and state

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
-import { ThemeProvider } from 'styled-components'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import GlobalStyle from '../style/GlobalStyle';
 import db from '../db.json';
 
-const theme = db.theme;
+const { theme } = db;
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>QuizJS</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
-      <GlobalStyle/>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -18,6 +20,17 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const router = useRouter();
+  function submit(e) {
+    e.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  }
+
+  function changeName(e) {
+    setName(e.target.value);
+  }
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -27,14 +40,20 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={submit}>
+              <input placeholder="Digite seu nome" onChange={changeName} />
+              <button type="submit" disabled={name.length === 0}>
+                Bora
+                {' '}
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
